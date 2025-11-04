706069912e2d82f02e9c8719226bf71d8504c305
Karina <kasantos0100@gmail.com>
2025-11-03 22:07:18 -0300

    Projeto part 1

--- agenda/core/tests/test_model_agenda.py
+++ agenda/core/tests/test_model_agenda.py
@@ -4,17 +4,17 @@ from core.models import Agenda
 class AgendaModelTest(TestCase):
     def setUp(self):
         self.agenda = Agenda.objects.create(
-            nome_completo="João da Silva",
+            nome_completo="Cassia Figuereido",
             telefone="(19) 99999-8888",
-            email="joao.silva@example.com",
+            email="cassia.figuereido@example.com",
             observacao="Cliente importante, prefere contato por e-mail."
         )
 
     def test_agenda_criada_com_sucesso(self):
-        self.assertEqual(self.agenda.nome_completo, "João da Silva")
+        self.assertEqual(self.agenda.nome_completo, "Cassia Figuereido")
         self.assertEqual(self.agenda.telefone, "(19) 99999-8888")
-        self.assertEqual(self.agenda.email, "joao.silva@example.com")
+        self.assertEqual(self.agenda.email, "cassia.figuereido@example.com")
         self.assertEqual(self.agenda.observacao, "Cliente importante, prefere contato por e-mail.")
 
     def test_str_retorna_nome_e_email(self):
-        self.assertEqual(str(self.agenda), "João da Silva - joao.silva@example.com")
+        self.assertEqual(str(self.agenda), "Cassia Figuereido - cassia.figuereido@example.com")
